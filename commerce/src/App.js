098450347277b9e1ead43dff0e9fc2e9154f9ca9
angel.gonzalez.dev.front <angel.gonzalez.dev.2020@gmail.com>
2022-07-06 21:23:21 -0400

    add routes

--- commerce/src/App.js
+++ commerce/src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import React from 'react'
 import './App.css';
-
+import { BrowserRouter, Routes, Route ,Navigate } from 'react-router-dom';
+import Beginning from './pages/beginning/Beginning';
+import ProductDetail from './pages/productDetail/productDetail';
+import Login from './pages/login/logIn';
+import CreateProduct from './pages/createProduct/createProduct';
+import PreviousPurchases from './pages/previousPurchases/previousPurchases';
+import Products from './pages/produts/products';
+import Nav from './components/nav/nav';
+import SignIn from './pages/signin/signIn';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <BrowserRouter>
+          <Nav />
+          <div className="App">
+              <Routes> 
+              {/* <Route path="/" element={<Navigate replace to="/home" />} /> */}
+                <Route 
+                  path='/'
+                  element={<Beginning />}
+                />
+                <Route 
+                  path='/login'
+                  element={<Login />}
+                />
+                <Route 
+                  path='/previousPurchases'
+                  element={<PreviousPurchases />}
+                />
+                <Route 
+                  path='/productDetail/:id'
+                  element={<ProductDetail />}
+                />
+                <Route 
+                  path='/products'
+                  element={<Products />}
+                />
+                <Route 
+                  path='/SignIn'
+                  element={<SignIn />}
+                />
+                <Route 
+                  path='/createProduct'
+                  element={<CreateProduct />}
+                />
+              </Routes>
+          </div>
+      </BrowserRouter>
   );
 }
 
 export default App;
+
+// {/* <header className="App-header">
+//   <img src={logo} className="App-logo" alt="logo" />
+//   <p>
+//     Edit <code>src/App.js</code> and save to reload.
+//   </p>
+//   <a
+//     className="App-link"
+//     href="https://reactjs.org"
+//     target="_blank"
+//     rel="noopener noreferrer"
+//   >
+//     Learn React
+//   </a>
+// </header> */}
