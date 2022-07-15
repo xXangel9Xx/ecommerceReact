79d7d0d45ac5122992acf41e073589979e0485a1
xXangel1Xx <108705159+xXangel1Xx@users.noreply.github.com>
2022-07-15 15:41:46 -0400

    Update products.jsx

--- commerce/src/pages/produts/products.jsx
+++ commerce/src/pages/produts/products.jsx
@@ -109,9 +109,9 @@ const Products = (props) => {
                         <div className="card-body">
                             <h5 className="card-title">{product.title}</h5>
                             <p className="card-text">{product.description}</p>
-                            <Link to={'/productDetail/'+product.slug }  className={"btn btn-outline-primary"}>
+                            <a href={'/productDetail/'+product.slug }  className={"btn btn-outline-primary"}>
                                     Ver detalles
-                            </Link>
+                            </a>
                         </div>
                     </div>
                     )
