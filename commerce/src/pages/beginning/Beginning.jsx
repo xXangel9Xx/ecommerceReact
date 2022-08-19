73e3293579cdfb0a73eb71d5a874b076de31455a
angel.gonzalez.dev.front <angel.gonzalez.dev.2020@gmail.com>
2022-08-19 14:16:34 -0400

    change line 108

--- commerce/src/pages/beginning/Beginning.jsx
+++ commerce/src/pages/beginning/Beginning.jsx
@@ -105,7 +105,7 @@ const Beginning = () => {
         {/* /////////////////////////////////////////////// Carousel //////////////////////////////////////////////// */}
                 <div id="carouselExampleDark" className="carousel carousel-dark slide" data-bs-ride="carousel">
                     <div className="carousel-indicators">
-                        <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+                        <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-href="0" className="active" aria-current="true" aria-label="Slide 1"></button>
                         <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
                         <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
                     </div>
